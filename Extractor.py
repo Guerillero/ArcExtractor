430714123adcae75f2d893f372c374506f8292fe
Tom Fish <guerillero.wikipedia@gmail.com>
2015-01-15 19:47:24 -0500

    standards

--- Extractor.py
+++ Extractor.py
@@ -8,65 +8,58 @@
 
 #Start dependency tree
 import arcpy
-from arcpy import env
 
-#declare map
+# declare map
 mxd = arcpy.mapping.MapDocument("CURRENT")
 
 
-#Import data from ArcMap
-fileToConvert = arcpy.GetParameterAsText(0)
-listFieldValues = arcpy.GetParameterAsText(1)
-outLocation = arcpy.GetParameterAsText(2)
-fieldName = arcpy.GetParameterAsText(3)
-#future place for field on what type of table to end up as
-spreadsheetType = arcpy.GetParameterAsText(4)
+# Import data from ArcMap
+fileToConvert = arcpy.GetParameterAsText(0) # 
+listFieldValues = arcpy.GetParameterAsText(1) #text file
+outLocation = arcpy.GetParameterAsText(2) #folder
+fieldName = arcpy.GetParameterAsText(3) # text field
+spreadsheetType = arcpy.GetParameterAsText(4) #text field -- Valid responses are dbf or txt
 
 if spreadsheetType == "dbf":
-	fileType = ".dbf"
-else if spreadsheetType == "csv":
-	fileType = ".txt"
-else if spreadsheetType == "txt":
-	fileType = ".txt"
-else
-	arcpy.AddMessage("ERROR: Not a valid file type")
+    fileType = ".dbf"
+elif spreadsheetType == "csv":
+    fileType = ".txt"
+elif spreadsheetType == "txt":
+    fileType = ".txt"
+else:
+    arcpy.AddMessage("ERROR: Not a valid file type")
 
-#translate file into layer
+# translate file into layer
 lyr = arcpy.mapping.Layer(fileToConvert)
 lyr.name = "MyFile"
 
-#declare array for UIDs
+# declare arrays for this script
 fieldValues = []
+fileName = []
+filePath = []
 
-#declare the file to open
+# declare the file to open
 fin = open (listFieldValues)
 
-#tell end user if there is an error
+# tell end user if there is an error
 if fin.closed:
-	arcpy.AddMessage("ERROR: File failed to open")
+    arcpy.AddMessage("ERROR: File failed to open")
 
 for line in fin:
-	#Turn each line in the file into an entry to the array
-	lineClear = str.strip ( line )
-	fieldValues.append(lineClear)
+    # Turn each line in the file into an entry to the array
+    lineClear = str.strip ( line )
+    fieldValues.append(lineClear)
 
-#declare array for file names
-
-fileName = []
-
-#make file names from UIDs
+# make file names from UIDs
 for i in range (len(fieldValues)):
         fileName.append (fieldValues[i] + "fileType")
-
-filePath = []
 
 for y in range (len(fileName)):
         filePath.append(outLocation + "\\" + fileName[y])
 
 for z in range (len(fieldValues)):
-	#Extract out only the entries that are wanted 
+    # Extract out only the entries that are wanted
     arcpy.AddMessage("Currently extracting "+ fieldValues[z] + " in " + fieldName)
     if lyr.name == "MyFile": 
         lyr.definitionQuery = fieldName + " =" + "'" + fieldValues[z] + "'"
-	arcpy.CopyRows_management(lyr, filePath[z])
-
+    arcpy.CopyRows_management(lyr, filePath[z])
